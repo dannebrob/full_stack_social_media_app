160ec6ce7f6910263ee08cedecfdee3a9c4d13c6
dannebrob <65211641+dannebrob@users.noreply.github.com>
2022-07-14 00:25:39 +0200

    implemented loading spinner

--- social_media_frontend/src/components/Feed.js
+++ social_media_frontend/src/components/Feed.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { client } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
 
 function Feed() {
-  return <div>Feed</div>;
+  const [loading, setLoading] = useState(false);
+  if (loading)
+    return <Spinner message="We are adding new ideas to your feed" />;
+  return (
+    <div>
+      <MasonryLayout />
+    </div>
+  );
 }
 
 export default Feed;
