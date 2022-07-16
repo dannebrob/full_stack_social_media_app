0e932f1224905e0fb1439127db992264ed65ad8d
dannebrob <65211641+dannebrob@users.noreply.github.com>
2022-07-16 16:21:04 +0200

    display pin functionality

--- social_media_frontend/src/components/MasonryLayout.js
+++ social_media_frontend/src/components/MasonryLayout.js
@@ -1,7 +1,28 @@
 import React from "react";
+import Masonry from "react-masonry-css";
+import Pin from "./Pin";
 
-function MasonryLayout() {
-  return <div>MasonryLayout</div>;
+const breakpointObj = {
+  default: 4,
+  3000: 6,
+  2000: 5,
+  1200: 3,
+  1000: 2,
+  500: 1,
+};
+function MasonryLayout({ pins }) {
+  return (
+    <div>
+      <Masonry
+        className="flex animate-slide-fwd"
+        breakpointCols={breakpointObj}
+      >
+        {pins?.map((pin) => (
+          <Pin key={pin.id} pin={pin} className="w-max" />
+        ))}
+      </Masonry>
+    </div>
+  );
 }
 
 export default MasonryLayout;
